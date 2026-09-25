de00a216db78d0062794d8dd743f46bcf73f6745
Jacob Scott <jscott2718@gmail.com>
2026-09-25 16:06:16 +0100

    feat: add :IpyVenv command

--- lua/jet/ipy/init.lua
+++ lua/jet/ipy/init.lua
@@ -2,6 +2,9 @@ local M = {}
 
 ---@class jet.ipy.Config
 local config = {
+	---If `true` (the default), a warning notification will be shown when
+	---opening a kernel without a virtual environment.
+	check_for_venv = true,
 	---Whether to prioritise kernels which live in a virtual environment
 	prioritise_venv = true,
 	---Ascend the tree until we hit one of these. Full list at
@@ -77,9 +80,33 @@ local get_python_expr = function(pos)
 	})
 end
 
+local setup_venv_cmd = function()
+	vim.api.nvim_create_user_command("IpyVenv", function(opts)
+		local venv = opts.fargs[1] or vim.fs.find(".venv", { type = "directory" })[1]
+		if venv then
+			vim.env.VIRTUAL_ENV = venv
+			local venv_bin = vim.fs.joinpath(venv, "bin")
+			if vim.uv.fs_stat(venv_bin) then
+				vim.env.PATH = venv_bin .. ":" .. vim.env.PATH
+				return
+			end
+		end
+		vim.notify("Virtual environment not found", vim.log.levels.ERROR)
+	end, { nargs = "?", complete = "dir" })
+end
+
+local did_setup = false
+
 ---@param opts? Partial<jet.ipy.Config>
 M.setup = function(opts)
 	opts = opts or {}
+
+	if did_setup then
+		return
+	end
+	did_setup = true
+
+	setup_venv_cmd()
 
 	local jet = require("jet")
 	assert(jet.did_setup(), 'jet.nvim has not done setup; run `require("jet").setup({})` before loading jet.ipy')
@@ -94,6 +121,24 @@ M.setup = function(opts)
 			if k.spec_path:match("%.venv/") then
 				k.priority = 200
 			end
+		end
+	end
+	if config.check_for_venv then
+		jet.hooks.on_lua_client_start.warn_not_venv = function(k)
+			if k.spec.display_name:lower():match("python") and not k.spec_path:match("%.venv/") then
+				vim.notify(
+					table.concat({
+						"Warning: starting Python kernel without a virtual environment",
+						"- Hint: use :IpyVenv to activate a virtual environment if one exists",
+					}, "\n"),
+					vim.log.levels.WARN
+				)
+			end
+		end
+	end
+	jet.hooks.on_kernel_init.set_python_ft = function(k)
+		if k.spec.display_name:lower():match("python") then
+			k.filetype = "python"
 		end
 	end
 end
